757540d962133aa7de0d33457b3fafdede126a77
missed86 <jimmy11286@gmail.com>
2023-02-06 08:40:42 +0100

    first commit

---
+++ mb_project/urls.py
@@ -0,0 +1,9 @@
+# mb_project/urls.py
+from django.contrib import admin
+from django.urls import path, include   # new
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('posts.urls')),    # new
+]
